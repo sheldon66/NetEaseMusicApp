ffe64d9beaebf6c056b3871df79fba638126b303
sheldon_66 <sheldon_66@163.com>
2020-12-28 13:04:39 +0000

    懒加载指令

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import store from './store'
 import '@/common/scss/reset.scss'
 
 Vue.config.productionTip = false
+Vue.directive('lazyload', {
+  bind: function (el, binding, vnode) {
+    el.src = require('@/assets/default.png')
+    el.onload = () => {
+      el.onload = null
+      el.src = binding.value
+    }
+  }
+})
 
 new Vue({
   router,
